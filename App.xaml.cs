1809aa7f326e5fc638bc93003dc5489f5bcce12f
Washington Yandun <washington.yandun06@gmail.com>
2022-11-22 21:36:06 -0500

    Add Login Page

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,24 @@
-﻿namespace Wise;
+﻿using Microsoft.Maui.Platform;
+using Wise.Handlers;
+
+namespace Wise;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+    public App()
+    {
+        InitializeComponent();
+
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(
+            nameof(WiseEntry), (handler, view) =>
+            {
+                if (view is WiseEntry)
+                {
+                    handler.PlatformView.SetBackgroundColor(Colors.Transparent.ToPlatform());
+                    // handler.NativeView.Text = view.Text;
+                }
+            });
+        MainPage = new AppShell();
+    }
 }
