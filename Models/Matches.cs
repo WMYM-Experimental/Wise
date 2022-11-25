8524831469fd5337bc27248e3fd1ed6f39eae9cc
Ivan Tulcan <ivan.tulcan@udla.edu.ec>
2022-11-24 22:16:49 -0500

    Updating layouts *login, signup, profilesetup

--- Models/Matches.cs
+++ Models/Matches.cs
@@ -1,10 +1,4 @@
-﻿using Java.Util.Functions;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Wise.Models
 {
---
+++ View/SignupView.xaml.cs
@@ -0,0 +1,9 @@
+namespace Wise.View;
+
+public partial class SignupView : ContentPage
+{
+	public SignupView()
+	{
+		InitializeComponent();
+	}
+}
