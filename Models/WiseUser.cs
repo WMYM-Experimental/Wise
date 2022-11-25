f5052f4c5be690b18d7ec7636b6d9cb28c8fa5fd
Washington Yandun <washington.yandun06@gmail.com>
2022-11-24 19:51:42 -0500

    Create User Model

---
+++ Models/WiseUser.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wise.Models
+{
+    public class User
+    {
+        #region Props
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public string? Location { get; set; }
+        public string? Bio { get; set; }
+        public int Elo { get; set; }
+        [Required]
+        public int Gender { get; set; }
+        public string? Nacionalidad { get; set; }
+        public int Sign { get; set; }
+        public int IsSmoker { get; set; }
+        public int IsDrinker { get; set; }
+        public string? University { get; set; }
+        public string? Work { get; set; }
+        public DateTime Birthday { get; set; }
+        public DateTime LastOnline { get; set; }
+
+        [Required]
+        public byte[] MainPic { get; set; }
+        [Required]
+        public string[] SexualPreferences { get; set; }
+        #endregion
+
+        #region Relationships
+        // Todo: Makea tabel of matchs an likes
+        #endregion
+    }
+}
