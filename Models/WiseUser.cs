3cd9e85c04f3dce3061d2ba3e06a183e798f6ada
Washington Yandun <washington.yandun06@gmail.com>
2022-11-24 19:58:50 -0500

    Refactor model

--- Models/WiseUser.cs
+++ Models/WiseUser.cs
@@ -2,28 +2,29 @@
 
 namespace Wise.Models
 {
-    public class User
+    public class WiseUser
     {
         #region Props
+
         [Key]
         public int Id { get; set; }
         [Required]
-        public string? Email { get; set; }
+        public string Email { get; set; }
         [Required]
-        public string? Password { get; set; }
+        public string Password { get; set; }
         [Required]
-        public string? Name { get; set; }
-        public string? Location { get; set; }
-        public string? Bio { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Bio { get; set; }
         public int Elo { get; set; }
         [Required]
         public int Gender { get; set; }
-        public string? Nacionalidad { get; set; }
+        public string Nacionalidad { get; set; }
         public int Sign { get; set; }
         public int IsSmoker { get; set; }
         public int IsDrinker { get; set; }
-        public string? University { get; set; }
-        public string? Work { get; set; }
+        public string University { get; set; }
+        public string Work { get; set; }
         public DateTime Birthday { get; set; }
         public DateTime LastOnline { get; set; }
 
@@ -31,6 +32,7 @@ namespace Wise.Models
         public byte[] MainPic { get; set; }
         [Required]
         public string[] SexualPreferences { get; set; }
+
         #endregion
 
         #region Relationships
