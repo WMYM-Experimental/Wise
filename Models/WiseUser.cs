54270e52bac713dd83503bcc65421496242184c4
Ivan Tulcan <ivan.tulcan@udla.edu.ec>
2022-11-24 20:47:43 -0500

    Models creation *likes, matches

--- Models/WiseUser.cs
+++ Models/WiseUser.cs
@@ -8,22 +8,22 @@ namespace Wise.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        public string? Email { get; set; }
+        public string Email { get; set; }
         [Required]
-        public string? Password { get; set; }
+        public string Password { get; set; }
         [Required]
-        public string? Name { get; set; }
-        public string? Location { get; set; }
-        public string? Bio { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Bio { get; set; }
         public int Elo { get; set; }
         [Required]
         public int Gender { get; set; }
-        public string? Nacionalidad { get; set; }
+        public string Nacionality { get; set; }
         public int Sign { get; set; }
         public int IsSmoker { get; set; }
         public int IsDrinker { get; set; }
-        public string? University { get; set; }
-        public string? Work { get; set; }
+        public string University { get; set; }
+        public string Work { get; set; }
         public DateTime Birthday { get; set; }
         public DateTime LastOnline { get; set; }
 
