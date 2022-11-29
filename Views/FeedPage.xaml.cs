979d7dfa0e9d345eb28b5f3442f0c44b52e91390
Ivan Tulcan <ivan.tulcan@udla.edu.ec>
2022-11-28 19:36:23 -0500

    Crea chatspage, feedpage, profilepage

---
+++ Views/FeedPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Wise.Views;
+
+public partial class FeedPage : ContentPage
+{
+	public FeedPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Views/ProfilePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Wise.Views;
+
+public partial class ProfilePage : ContentPage
+{
+	public ProfilePage()
+	{
+		InitializeComponent();
+	}
+}
