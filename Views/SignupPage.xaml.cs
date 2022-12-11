4a71fc8c5b89111d71008ea97bbd6e3af95f5298
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2022-12-11 13:14:44 -0500

    Start implementation of file for signup

--- Views/SignupPage.xaml.cs
+++ Views/SignupPage.xaml.cs
@@ -1,9 +1,42 @@
+using Wise.Models;
+
 namespace Wise.Views;
 
 public partial class SignupPage : ContentPage
 {
-	public SignupPage()
-	{
-		InitializeComponent();
-	}
+    //public SignupPage()
+    //{
+    //    InitializeComponent();
+
+    //    string appDataPath = FileSystem.AppDataDirectory;
+    //    string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+    //    LoadSignup(
+    //        fileName: Path.Combine(appDataPath, randomFileName)
+    //        );
+    //}
+
+    //private void LoadSignup(string fileName)
+    //{
+    //    WiseUser signupUserFile = new();
+    //    signupUserFile.Name = fileName;
+
+    //    if (File.Exists(fileName))
+    //    {
+    //        signupUserFile.LastOnline = File.GetCreationTime(fileName);
+    //        signupUserFile.Bio = File.ReadAllText(fileName);
+    //    }
+
+    //    BindingContext = signupUserFile;
+    //}
+
+    //private async void SaveButton_Clicked(object sender, EventArgs e)
+    //{
+    //    if (BindingContext is WiseUser wuser)
+    //    {
+    //        File.WriteAllText(note.Filename, Email.Text);
+    //    }
+
+    //    await Shell.Current.GoToAsync("FeedPage");
+    //}
 }
