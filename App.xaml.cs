9bd0e42f1a62d50afbc7718f23a326e2145a07f3
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2022-12-11 00:53:56 -0500

    Change appshell for tests

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,15 +9,6 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
-        //Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(
-        //    nameof(WiseEntry), (handler, view) =>
-        //    {
-        //        if (view is WiseEntry)
-        //        {
-        //            handler.PlatformView.SetBackgroundColor(Colors.Transparent.ToPlatform());
-        //            // handler.NativeView.Text = view.Text;
-        //        }
-        //    });
         MainPage = new AppShell();
     }
 }
