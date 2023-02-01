42900837b0a58cfb746bb708d5338bb0d713a0c9
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2023-01-31 22:05:40 -0500

    Create Services for post a match

---
+++ Services/ApisService.cs
@@ -0,0 +1,32 @@
+﻿namespace Wise.Services
+{
+    public class ApisService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApisService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            return await _httpClient.GetAsync(url);
+        }
+
+        public async Task<HttpResponseMessage> PostAsync(string url, HttpContent content)
+        {
+            return await _httpClient.PostAsync(url, content);
+        }
+
+        public async Task<HttpResponseMessage> PutAsync(string url, HttpContent content)
+        {
+            return await _httpClient.PutAsync(url, content);
+        }
+
+        public async Task<HttpResponseMessage> DeleteAsync(string url)
+        {
+            return await _httpClient.DeleteAsync(url);
+        }
+    }
+}
---
+++ Services/MatchesService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Wise.Models;
+
+namespace Wise.Services
+{
+    public class MatchesService
+    {
+        public MatchesService()
+        {
+            
+        }
+
+        public async Task<List<WiseUser>> ObtenerDatosUsuarios()
+        {
+            using var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://localhost:7086/api/users");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Error al obtener los datos de los usuarios");
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var usuarios = JsonSerializer.Deserialize<List<WiseUser>>(json);
+            return usuarios;
+        }
+        
+        //create a function for creating a match and send by post to an api
+        public Match CreateMatch(int idPerson, int idMatch)
+        {
+            var match = new Match(idPerson, idMatch, true);
+            return match;
+        }
+        
+    }
+}
